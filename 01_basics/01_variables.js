9fde481c200d7e6db19b552bb68b6cac3168b967
Student-Pwskills <166196890+Student-Pwskills@users.noreply.github.com>
2024-05-30 15:06:13 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 2635
+let accountEmail = "vaibhavshukla78@gmail.com"
+var accountPassword = "125"
+accountCity = "jaipur"
+//accountId = 23
+// console.log(accountId)
+// console.log(accountPassword)
+// console.log(accountEmail)
+console.table([accountEmail,accountId,accountPassword])

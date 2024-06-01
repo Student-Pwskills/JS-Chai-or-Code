a67d6d78f5ec18cdf493e0bdfc005f4574406c6c
Student-Pwskills <166196890+Student-Pwskills@users.noreply.github.com>
2024-06-01 03:04:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "vaibhav"      //"33abc" ,null,undefined,true,"vaibhav"
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+//Notes :
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+//  true => 1 and false => 0
+ 
+let isLoggedIn = "vaibhav"      // 0,1,"","anyname"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//Notes:
+// 1 => 0, 0 => 1 
+// "" => false , "nonEmptyString" => true
+
+let someNumber = 45
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)

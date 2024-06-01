adb9045f73c3926174799536ab228a569580b882
Student-Pwskills <166196890+Student-Pwskills@users.noreply.github.com>
2024-06-01 01:41:40 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,4 +1,24 @@
 "use strict"; // treat all js code as newer version
 alert(3+3) // we are using nodejs not browser
-console.log(3+3) 
+
+console.log(3+3)    //code readability should be high
 console.log("Vaibhav")
+
+let name = "vaibhav" //string datatype
+let age = 20 // number
+let isLoggedIn = false // boolean datatype
+
+//some datatypes 
+// number => 2 to the power 53 (range)
+// bigint 
+// string => ""
+// boolean
+// null => standalone value
+// undefined =>
+// symbol => unique 
+// these above datatypes are primitive datatypes
+// object
+
+console.log(typeof "Vaibhav")
+console.log(typeof null)
+console.log(typeof undefined)

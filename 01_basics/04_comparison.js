382dea6f719185b89bec8f9809f7d4bd9916292a
Student-Pwskills <166196890+Student-Pwskills@users.noreply.github.com>
2024-06-02 06:12:58 +0000

    comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1) //"2" is converted into number then compared
+// console.log("02">1)
+
+console.log(null>0)
+console.log(null==0)  // it might give unpredictable result so avoid it.
+console.log(null>=0) // here "==" is quite different from ">" operator
+
+console.log("2"===2) // it checks both operands as well as their datatypes 

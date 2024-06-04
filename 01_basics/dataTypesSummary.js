224d1889ac0450fd9a274706f31a20028f25a8ef
Student-Pwskills <166196890+Student-Pwskills@users.noreply.github.com>
2024-06-04 13:53:26 +0000

    stack and heap added

--- 01_basics/dataTypesSummary.js
+++ 01_basics/dataTypesSummary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -29,9 +29,30 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("Hello world");
+   // console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++++++++ Stack and Heap ++++++++++++++++++++++++
+ // Stack(stores primitive values) and Heap(stored non primitive values)
+ // primitive values gives copy of orginal values
+ // Non primitive values gives reference of original values
+  
+ let myNameIs = "Vaibhav"
+ let anotherNameIs = myNameIs
+ //console.log(anotherNameIs)
+ anotherNameIs = "Chhotu"
+//  console.log(anotherNameIs)
+//  console.log(myNameIs)
+
+ let userOne = {
+    email:"vaibhav@google",
+    upi: "#fad@"
+ }
+ userTwo = userOne
+ userTwo.email="chhotu@google.com"
+ console.log(userOne.email)
+ console.log(userTwo.email)

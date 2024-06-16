50d35677464d79054882daca208745ec93153178
Student-Pwskills <166196890+Student-Pwskills@users.noreply.github.com>
2024-06-16 16:05:58 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,3 +12,39 @@ if(true){
 console.log(a)
 //console.log(b)
 console.log(c)
+
+function one(){
+    const name = "vaibhav"
+    function two(){
+     const website = "youtube"
+     console.log(name)
+    }
+  //  console.log(website)
+    two()
+}
+one()
+
+if(true){
+    const name = "vaibhav"
+    if( name == "vaibhav"){
+        const website = " youtube"
+        console.log( name + website)
+    }
+   // console.log(website)
+}
+//console.log(name)
+
+
+//++++++++++++++++++++++++++++ Intresting +++++++++++++++++++++++++++++
+
+// addOne(5)
+function addOne(num){
+    return num + 1
+}
+addOne(5)
+ 
+// addTwo(5)
+const addTwo = function( num){
+    return num + 2
+}
+addTwo(5)
